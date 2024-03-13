fe279d74479ae6ae45eed180eb49170c24a4bda7
cubo1991 <adavidlopezmathez@gmail.com>
2024-03-12 22:09:03 -0300

    endpoint all pokemon

--- api/controllers/pokemonAll.js
+++ api/controllers/pokemonAll.js
@@ -2,19 +2,39 @@ const controller = {}
 const axios = require('axios');
 
 controller.index = (req, res) => {
-    axios.get('https://pokeapi.co/api/v2/pokemon/')
+
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 21;
+    const offSet = (page - 1) * limit;
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offSet}`)
     .then(response => {
       let results = response.data.results;
      
-      let pokemons = results.map(pokemon => {
-            return {
-                nombre: pokemon.name,
-                url: pokemon.url
-            };
-      })
+      let promises = results.map(pokemon => {
+            return axios.get(pokemon.url)
+            .then(response => {
+                
+                console.log(pokemon.name)
+                               
+                return {
+                    name: pokemon.name,
+                    data: response.data
+                };
+            })
+            .catch(error => {
+                console.error(error);
+            });
+      });
 
-      console.log(pokemons);
-      res.send(pokemons);
+      Promise.all(promises)
+      .then(pokemons => {
+         
+          res.send(pokemons);
+      })
+      .catch(error => {
+          console.error(error);
+      });
     })
     .catch(error => {
       console.error(error);
