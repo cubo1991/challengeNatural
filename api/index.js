7c4e4eb6cad86a792dd1ec8b57c4e1ff09967820
cubo1991 <adavidlopezmathez@gmail.com>
2024-03-12 22:14:46 -0300

    front iniciado

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const routes = require('./routes/index.js');
 const morgan = require('morgan');
 
 const server = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 //middlewares
 server.use(morgan('dev'));
